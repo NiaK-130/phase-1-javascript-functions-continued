cf980777e169e07a40a2de1bb5c97971d59ea5ae
Nia K <nsjkappen@gmail.com>
2021-07-22 14:58:28 -0700

    commit tree dd714e8f6a725e6b0bbc60713246e00382cb8b32

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+function saturdayFun(activity){
+    if (activity === undefined){
+        return `This Saturday, I want to roller-skate!`
+    } else {
+        return `This Saturday, I want to ${activity}!`
+    }  
+}
+
+//----------------------------------------------------------//
+
+
+function mondayWork(location){
+    if (location === undefined){
+        return 'This Monday, I will go to the office.';
+    } else {
+        return `This Monday, I will ${location}.`
+    }
+}
+
+
+//----------------------------------------------------------//
+
+function wrapAdjective(name){
+
+    const part1 = "special"
+    return `You are special`
+    
+
+}
+    
+
+function wrapAdjective(flair = '*') {
+    return function (adj = 'special') {
+        return `You are ${flair}${adj}${flair}!`;
+    }
+}
+
+const promptFunction = wrapAdjective('!!!')
